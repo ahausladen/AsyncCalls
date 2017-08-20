692ea4f07d2eba538b9f287933c78f4f4a008e3b
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2017-08-20 14:28:50 +0200

    Fixed typo

--- AsyncCalls.pas
+++ AsyncCalls.pas
@@ -2880,7 +2880,7 @@ asm
   { Detect if the finally block is called by System._HandleFinally.
     In that case an exception was raised in the MainThread-Block. The
     Classes.CheckSynchronize function will handle the exception and the
-    thread switch for us. This will also restore the EBP regíster. }
+    thread switch for us. This will also restore the EBP register. }
   mov eax, [esp + $04] // finally return address
   mov edx, OFFSET System.@HandleFinally
   cmp eax, edx
